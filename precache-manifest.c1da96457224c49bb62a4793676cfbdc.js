f9408a4c4679c1d0b74db9b7cc5f04106ea939b6
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-04-27 18:11:04 +0545

    Updates

---
+++ precache-manifest.c1da96457224c49bb62a4793676cfbdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba8c57b465ecd1f5a1ff10cccf93441",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "439bcd00da1df47052e3",
+    "url": "/covid-19-tracker/static/css/main.4fc4adbc.chunk.css"
+  },
+  {
+    "revision": "7f2b699c099b0dcd8625",
+    "url": "/covid-19-tracker/static/js/2.cc6b80c5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.cc6b80c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439bcd00da1df47052e3",
+    "url": "/covid-19-tracker/static/js/main.4b44f0ff.chunk.js"
+  },
+  {
+    "revision": "f5a2e4780e5e49bf93c5",
+    "url": "/covid-19-tracker/static/js/runtime-main.92976cc4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/logo.d7265326.png"
+  }
+]);
